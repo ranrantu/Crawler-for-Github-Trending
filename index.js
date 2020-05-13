97b02e3763cbbb86237949c3e414667e4b9b0894
ranrantu <isez2016@gmail.com>
2020-05-14 02:10:18 +0800

    爬虫支持多城市下载

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const app = express()
 const nodeExcel = require('node-xlsx');
 const urlencode = require('urlencode');
 
-function getData(time, language) {
-    let url = 'http://supin.58.com/pc/list';  // 拼接请求的页面链接
+const cityInfoList = [
+    { cityName: '北京', cityId: 1 },
+    { cityName: '上海', cityId: 2 },
+    { cityName: '广州', cityId: 3 },
+    { cityName: '深圳', cityId: 4 },
+    { cityName: '杭州', cityId: 79 },
+    { cityName: '厦门', cityId: 606 },
+]
+
+function getData({cityId, cityName}) {
+    let url = `http://supin.58.com/pc/list?city=${cityId}`;  // 拼接请求的页面链接
     return axios.get(url)
         .then(function (response) {
             let html_string = response.data.toString(); // 获取网页内容
@@ -20,7 +29,7 @@ function getData(time, language) {
             });
 
             return Promise.all(fetchList).then((data) => {
-                return data;
+                return { cityName, data };
             }).catch(() => {});
 
         })
@@ -74,14 +83,6 @@ const getJobDetail = function (jobid) {
     });
 }
 
-app.get('/', (req, res) => {
-    // let promise = getData('daily'); // 发起抓取
-    // promise.then(response => {
-    //     res.json(response); // 数据返回
-    // });
-    res.end('<a href="/download">下载excle</a>')
-})
-
 const config = {
     job_name: '职位名称',
     job_salary: '岗位薪资',
@@ -91,55 +92,63 @@ const config = {
 };
 
 const exportExcel = function (res, obj) {
-    let excelConfig = [];
-    const objArray = Array.prototype.slice.call(obj);
-    // 标题部分
-    let titleArray = [];
-    Object.keys(objArray[0]).forEach(key => {
-        if (key === 'job_item') {
-            objArray[0][key].forEach(zItem => {
-                titleArray.push(zItem.title.replace('：', ''));
-            });
-        } else {
-            titleArray.push(config[key].replace('：', ''));
-        }
-    });
-    excelConfig.push(titleArray);
-    // 内容部分
-    objArray.forEach(item => {
-        let contentArray = [];
-        Object.keys(item).forEach(key => {
+    let excelData = [];
+    obj.forEach(({ cityName, data }) => {
+        let excelConfig = [];
+        const objArray = Array.prototype.slice.call(data);
+        // 标题部分
+        let titleArray = [];
+        Object.keys(objArray[0]).forEach(key => {
             if (key === 'job_item') {
-                item[key].forEach(zItem => {
-                    contentArray.push(zItem.content);
+                objArray[0][key].forEach(zItem => {
+                    titleArray.push(zItem.title.replace('：', ''));
                 });
             } else {
-                contentArray.push(item[key]);
+                titleArray.push(config[key].replace('：', ''));
             }
         });
-        excelConfig.push(contentArray);
-        console.log(contentArray)
+        excelConfig.push(titleArray);
+        // 内容部分
+        objArray.forEach(item => {
+            let contentArray = [];
+            Object.keys(item).forEach(key => {
+                if (key === 'job_item') {
+                    item[key].forEach(zItem => {
+                        contentArray.push(zItem.content);
+                    });
+                } else {
+                    contentArray.push(item[key]);
+                }
+            });
+            excelConfig.push(contentArray);
+        });
+        excelData.push({ name: `速聘_爬虫_${cityName}`, data: excelConfig });
     });
-    const excelName = '速聘-爬虫';
-    let buffer = nodeExcel.build([{name: excelName, data: excelConfig}]);
+    let buffer = nodeExcel.build(excelData);
     res.setHeader('Content-Type', 'application/octet-stream');
      // ctx.request.headers['user-agent']
-    let name = urlencode(excelName + '_' + (+new Date()) + '.xlsx', "utf-8");
+    let name = urlencode('速聘_爬虫_' + (+new Date()) + '.xlsx', "utf-8");
     res.setHeader("Content-Disposition", "attachment; filename* = UTF-8''"+name);    
     // res.setHeader("Content-Disposition", "attachment; filename="+ (+new Date()) + '.xlsx');
     res.end(buffer);
 }
 
+app.use(express.static('static'));
+
 app.get('/download', (req, res) => {
-    let promise = getData('daily');
-    promise.then(response => {
+    let list = cityInfoList.map(item => getData(item));
+    Promise.all(list).then(response => {
         exportExcel(res, response);
     });
 })
 
-app.get('/:time', (req, res) => {
+app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    res.end('<a href="/download">下载excle</a>')
+    // const optionsString = options.map((item, index) => `<option value='${item.cityId}'${index===0?' selected':''}>${item.cityName}</option>`).join('');
+    res.write(`<h1>速聘爬虫（包括${cityInfoList.map(({ cityName }) => cityName)}）</h1>`)
+    res.write(`<script src="./script.js"></script>`);
+    // res.write(`<select id="select">${optionsString}</select>`)
+    res.end(`<button class="button">下载excle</button>`)
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'))  // 监听3000端口
